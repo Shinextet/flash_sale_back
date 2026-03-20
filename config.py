64854bf54650ac37cee600a3ded6854b8b58920e
Shinextet <147969251+Shinextet@users.noreply.github.com>
2026-03-20 23:57:44 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+# Render ရဲ့ Environment Variables ထဲမှာ ထည့်ထားရင် ပိုကောင်းပါတယ်
+# မဟုတ်ရင် ဒီမှာ တိုက်ရိုက် String နဲ့ ထည့်နိုင်ပါတယ်
+TOKEN = "8416763005:AAGtGL5UuzTuG9_Glaqd5zKwndkAVdBbPvY"
+ADMIN_ID = 5965428729  # သင့်ရဲ့ Telegram User ID
+ADMIN_PAYMENT_INFO = """
+💰 **ငွေပေးချေရန် အချက်အလက်**
+KPay: 09775649834 (Shine Htet Aung)
+Wave: 09775649834 (Shine Htet Aung)
+AYAPay: 09775649834 (Shine Htet Aung)
+"""
+SUPABASE_URL = "https://onenfdaddikacglvayvg.supabase.co"
+SUPABASE_KEY = "sb_publishable_H4vO1GVW223MHhBz6wxTvA_g6rd9o2J"
